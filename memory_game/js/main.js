47617c343e511c804096aefa1c8977f468a8f622
Sam Nottingham <jnottin92@gmail.com>
2018-09-23 09:41:38 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "queen", "queen", "king", "king"];
 
-console.log("User flipped: " + cardOne)
-console.log("User flipped: " + cardTwo)
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped a " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped a " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	}
+	else {
+		alert("Sorry, try again.")
+	}
+}
