55c1e8ef905709cbf4f1041e4adc029f7d90fa82
Sam Nottingham <jnottin92@gmail.com>
2018-09-29 09:03:18 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,6 +23,16 @@ cardImage: "images/king-of-diamonds.png"
 
 var cardsInPlay = [];
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('gameboard').appendChild(cardElement);
+	}
+}
+
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -35,15 +45,13 @@ var checkForMatch = function() {
 
 
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+createBoard();
